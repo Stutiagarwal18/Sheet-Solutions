be81ec4c6a56f94e5d41224d1a97eb6f4094465a
Stuti Agarwal <stutiagarwal52@gmail.com>
2025-08-21 18:18:45 +0530

    Delete divisible.py

--- divisible.py
+++
@@ -1,7 +0,0 @@
-"""	Take an integer A as input. You have to tell whether A is divisible by both 5 and 11 or not."""
-
-A= int(input("enter the number"))
-if A% 5 == 0 and A%11==0:
-    print("yes divisible by both 5 and 11")
-else:
-    print("not divisible")
