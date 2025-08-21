b8e2f407ea8fd33fb0b33af8cd19d2acf6d97dd7
Stuti Agarwal <stutiagarwal52@gmail.com>
2025-08-21 18:20:35 +0530

    Delete oddNo.py

--- oddNo.py
+++
@@ -1,9 +0,0 @@
-"""  Write a program to print all odd numbers from 1 to N, where you have to take N as input 
-from user. """
-
-N= int(input("enter the number:"))
-i= 1
-while i<=N:
-    print(i)
-    i +=2
-print()
